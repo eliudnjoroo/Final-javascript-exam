5ef16e76e64bea3d543431e858f0c99a25b43b55
Eliud <124616502+eliudnjoroo@users.noreply.github.com>
2025-03-12 19:02:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,64 +12,107 @@ const def = document.getElementById('defense');
 const sa = document.getElementById('special-attack');
 const hp = document.getElementById('hp');
 const speed = document.getElementById('speed');
+const cont = document.getElementById("cont");
+const div = document.querySelectorAll("div");
 
 let nameOrId;
 let inputed;
 
 let pData;
 
-async function poke(url){
-  try{
-  const res = await fetch(url);
-  pData = await res.json();
-  }catch(e){
+async function poke(url) {
+  try {
+    const res = await fetch(url);
+    pData = await res.json();
+  }catch(e) {
+    cont.style.backgroundColor = "black";
+    cont.style.height = "0";
     alert("Pokémon not found");
+    location.reload();
   }
-  pName.textContent = pData.name.toUpperCase();
-  pId.textContent = "#"+pData.id
+  pName.textContent = "pokemon-name: "+pData.name.toUpperCase();
+  pId.textContent = "pokemon-id: #"+pData.id
   height.textContent = "height: "+pData.height
   weight.textContent = "weight: "+pData.weight
   const sprite = pData.sprites
-  let mysrc= sprite.front_default;
-  const cont = document.getElementById("cont");
-  cont.innerHTML =`<img src="${mysrc}" id="sprite">`
-  myTypes(pData.types)
-  stats(pData.stats);
+  let mysrc = sprite.front_default;
+
+  let t1;
+  let t2;
+  let t3;
+  let t4;
+  let id = setInterval (()=> {
+    t1 = setTimeout(function() {
+      cont.innerHTML = `<p>loading img.</p>`
+    }, 200);
+    t2 = setTimeout(function() {
+      cont.innerHTML = `<p>loading img..</p>`
+    }, 400);
+    t3 = setTimeout(function() {
+      cont.innerHTML = `<p>loading img...</p>`
+    }, 600);
+    t4 = setTimeout(function() {
+      cont.innerHTML = `<p>loading img....</p>`
+    }, 800);
+  }, 1000);
+  cont.innerHTML = `<img hidden src="${mysrc}" id="sprite">`
+  const sprite2 = document.getElementById("sprite");
+  sprite2.addEventListener("load", () => {
+    setTimeout(() => {
+      clearTimeout(t1);
+      clearTimeout(t2);
+      clearTimeout(t3);
+      clearTimeout(t4);
+      clearInterval(id);
+      div.forEach(d => d.style.minWidth = "40vw");
+      setTimeout(()=> {
+        myTypes(pData.types)
+        stats(pData.stats);
+        cont.innerHTML = `<img src="${mysrc}" id="sprite2">`;
+      }, 0);
+    }, 4000);
+  });
 }
 
 const stats = stats => {
   let arr = [];
-  stats.forEach( st =>{
-  let data = `${st.base_stat}`;
+  stats.forEach(st => {
+    let data = `${st.base_stat}`;
     arr.push(data);
   })
-  hp.textContent = arr[0];
-  attack.textContent = arr[1];
-  def.textContent = arr[2];
-  sa.textContent = arr[3];
-  sd.textContent = arr[4];
-  speed.textContent = arr[5];
+  hp.textContent = "hp: "+arr[0]+"hp";
+  attack.textContent = "attack: "+arr[1];
+  def.textContent = "defense: "+arr[2];
+  sa.textContent = "special-attack: "+arr[3];
+  sd.textContent = "special-defense: "+arr[4];
+  speed.textContent = "speed: "+arr[5]+"km/h";
 }
 
 const myTypes = data => {
   types.innerHTML = "";
   let ty = [];
-  for(let dat in data){
+  for (let dat in data) {
     ty.push(`<span class="type">${data[dat].type.name.toUpperCase()}</span>`);
   }
   ty.forEach(el => types.innerHTML += el);
 }
 
-searchBtn.addEventListener("click",e => {
+searchBtn.addEventListener("click", e => {
   e.target.classList.remove("anim");
   void e.target.offsetWidth;
   e.target.classList.add("anim")
   inputed = searchInput.value;
   nameOrId = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${inputed}`;
-  if(searchInput.value === ""){
+  if (searchInput.value === "") {
+    div.forEach(d => d.style.minWidth = "0vw");
     alert("please enter a name or id");
-  }else{
-     let toSearch = nameOrId.toLowerCase();
-     poke(toSearch);
+    cont.style.height = 0;
+    cont.style.backgroundColor = "black";
+  } else {
+    div.forEach(d => d.style.minWidth = "0vw");
+    cont.style.height = "45vw";
+    cont.style.backgroundColor = "transparent";
+    let toSearch = nameOrId.toLowerCase();
+    poke(toSearch);
   }
 })
